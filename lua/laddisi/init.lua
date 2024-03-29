21f24f6ec167263bafe77122fbfe108eb4bfc5a9
lucas <lucas.addisi@gmail.com>
2024-03-29 20:36:44 -0300

    Refactor directory name

---
+++ lua/laddisi/init.lua
@@ -0,0 +1,9 @@
+-- Get the path to the directory containing init.lua
+local script_dir = vim.fn.stdpath('config')
+
+-- Concatenate the path to .vimrc file
+local vimrc_path = script_dir .. '/.vimrc'
+
+-- Source the .vimrc file
+vim.cmd('source ' .. vimrc_path)
+
