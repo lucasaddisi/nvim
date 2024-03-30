a2d77d4faa66b809caf797b4f988311c70199c5a
lucas <lucas.addisi@gmail.com>
2024-03-30 01:02:43 -0300

    Add treesitter config

--- lua/laddisi/plugins/treesitter.lua
+++ lua/laddisi/plugins/treesitter.lua
@@ -1,4 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate"
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {"lua", "vim", "vimdoc", "query", "java", "python", "markdown", "json", "bash"}
+		})
+	end
 }
+
