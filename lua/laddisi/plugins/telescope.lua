a6726584498a5b0340d4f9a7c653bca41ee272ce
lucas <lucas.addisi@gmail.com>
2024-03-31 02:27:27 -0300

    Add git support in telescope

--- lua/laddisi/plugins/telescope.lua
+++ lua/laddisi/plugins/telescope.lua
@@ -19,5 +19,7 @@ return {
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		vim.keymap.set('n', '<leader>fgb', builtin.git_branches, {})
+		vim.keymap.set('n', '<leader>fgs', builtin.git_status, {})
 	end
 }
