1359de85c6f4b07bc9b57cedcd17ec0dfd0b7b19
lucas <lucas.addisi@gmail.com>
2024-03-31 01:44:34 -0300

    Search hidden files and ignore .git dirs

--- lua/laddisi/plugins/telescope.lua
+++ lua/laddisi/plugins/telescope.lua
@@ -5,9 +5,17 @@ return {
         	"nvim-lua/plenary.nvim"
     	},
 	config = function()
-		require('telescope').setup({})
+		require('telescope').setup({
+			defaults = {
+				file_ignore_patterns = {"^%.git/", "^node_modules/"}
+			}
+		})
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>ff',
+				function()
+					builtin.find_files({hidden=true, ignore=true})
+				end,
+				{})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
