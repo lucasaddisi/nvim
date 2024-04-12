fff799aa83130a9740abeaf7ea4a165b22ac91d0
Lucas Addisi <lucas.addisi@gmail.com>
2024-04-12 16:29:03 -0300

    Add Mason icons

--- lua/laddisi/plugins/mason.lua
+++ lua/laddisi/plugins/mason.lua
@@ -1,5 +1,15 @@
 return {
     "williamboman/mason.nvim",
-    config = function() require("mason").setup() end
+    config = function()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        }) 
+    end
 }
 
