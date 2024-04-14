6b1a5a76fddb8929b7b11143644def4f3fe568bc
lucas <lucas.addisi@gmail.com>
2024-04-14 17:49:23 -0300

    Add more configuration for jdtls

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,4 +1,11 @@
+-- Follow https://github.com/mfussenegger/nvim-jdtls and https://github.com/eclipse-jdtls/eclipse.jdt.ls to configure
+local cache_vars = {}
+
 local function get_jdtls_paths()
+    if cache_vars.paths then
+        return cache_vars.paths
+    end
+
     local path = {}
 
     local jdtls_install = require('mason-registry')
@@ -7,6 +14,7 @@ local function get_jdtls_paths()
 
     path.java_agent = jdtls_install .. '/lombok.jar'
     path.launcher_jar = vim.fn.glob(jdtls_install .. '/plugins/org.eclipse.equinox.launcher_*.jar')
+    path.data_dir = vim.fn.stdpath('cache') .. '/nvim-jdtls'
 
     if vim.fn.has('mac') == 1 then
         path.platform_config = jdtls_install .. '/config_mac'
@@ -16,17 +24,72 @@ local function get_jdtls_paths()
         path.platform_config = jdtls_install .. '/config_win'
     end
 
-    path.exec = jdtls_install .. '/bin/jdtls'
+    path.bundles = {}
+
+    ---
+    -- Include java-test bundle if present
+    ---
+    local java_test_path = require('mason-registry')
+    .get_package('java-test')
+    :get_install_path()
+
+    local java_test_bundle = vim.split(
+    vim.fn.glob(java_test_path .. '/extension/server/*.jar'),
+    '\n'
+    )
+
+    if java_test_bundle[1] ~= '' then
+        vim.list_extend(path.bundles, java_test_bundle)
+    end
+
+
+    cache_vars.paths = path
 
     return path
 end
 
-local jdtls_exec = get_jdtls_paths().exec
-print(jdtls_exec)
+local path = get_jdtls_paths()
+local data_dir = path.data_dir .. '/' ..  vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+print('Project specific data directory: ' .. data_dir)
+
+local cmd = {
+    -- 💀
+    'java',
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-javaagent:' .. path.java_agent,
+    '-Xms1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+
+    -- 💀
+    '-jar',
+    path.launcher_jar,
+
+    -- 💀
+    '-configuration',
+    path.platform_config,
+
+    -- 💀
+    '-data',
+    data_dir,
+}
+
+
+-- See https://github.com/eclipse-jdtls/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request 
+local settings = {}
 
 local config = {
-    cmd = {jdtls_exec},
+    cmd = cmd,
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    settings = settings
 }
 
 require('jdtls').start_or_attach(config)
