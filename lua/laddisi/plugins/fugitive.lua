b944b9dd90dedfc1970348b70e3ec92f7a26e8bc
Lucas Addisi <lucas.addisi@gmail.com>
2024-06-05 01:06:22 -0300

    Add comments

--- lua/laddisi/plugins/fugitive.lua
+++ lua/laddisi/plugins/fugitive.lua
@@ -2,7 +2,8 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set({'n', 'v'}, "<leader>gb", "<Cmd>Git blame<CR>")
-        vim.keymap.set({'n', 'v'}, "<leader>gd", "<Cmd>Gvdiffsplit<CR>")
+        vim.keymap.set({'n', 'v'}, "<leader>gd", "<Cmd>Gvdiffsplit<CR>") -- Opens vimdiff. dp (diffput) do (diffobtain)
         vim.keymap.set({'n', 'v'}, "<leader>gad", "<cmd>Git! difftool<cr>")
     end
 }
+
