e058b4507aae0eb0f3f7eec141cf69ab89b2ad40
Lucas Addisi <lucas.addisi@gmail.com>
2024-07-04 11:16:11 -0300

    Rename using visual mode

--- lua/laddisi/plugins/lspconfig.lua
+++ lua/laddisi/plugins/lspconfig.lua
@@ -25,7 +25,7 @@ return {
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, opts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set({ 'n', 'v' }, '<leader>rn', vim.lsp.buf.rename, opts)
                 vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                 vim.keymap.set('n', '<leader>=', function()
