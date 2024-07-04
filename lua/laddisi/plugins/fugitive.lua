02972d175f4993a4d128a0c205caa918e7e5eaf2
Lucas Addisi <lucas.addisi@gmail.com>
2024-07-04 16:33:11 -0300

    Open git on browser

--- lua/laddisi/plugins/fugitive.lua
+++ lua/laddisi/plugins/fugitive.lua
@@ -1,9 +1,13 @@
 return {
     "tpope/vim-fugitive",
+    dependencies = {
+        "tpope/vim-rhubarb"
+    },
     config = function()
         vim.keymap.set({'n', 'v'}, "<leader>gb", "<Cmd>Git blame<CR>")
         vim.keymap.set({'n', 'v'}, "<leader>gd", "<Cmd>Gvdiffsplit<CR>") -- Opens vimdiff. dp (diffput) do (diffobtain)
-        vim.keymap.set({'n', 'v'}, "<leader>gad", "<cmd>Git! difftool<cr>")
+        vim.keymap.set({'n', 'v'}, "<leader>gad", "<Cmd>git! difftool<cr>") -- g(it) a(ll) d(differences)
+        vim.keymap.set({'n', 'v'}, "<leader>go", "<Cmd>GBrowse<cr>") -- g(it) o(open) - Open file in the browser 
     end
 }
 
