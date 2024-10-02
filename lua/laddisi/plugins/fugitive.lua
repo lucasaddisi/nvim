5021acc1bbdaac35dfc75b1b6065df7cbe950f35
Lucas Addisi <lucas.addisi@gmail.com>
2024-10-02 19:13:27 -0300

    Remove git commit shortcut

--- lua/laddisi/plugins/fugitive.lua
+++ lua/laddisi/plugins/fugitive.lua
@@ -7,7 +7,6 @@ return {
         vim.keymap.set({'n', 'v'}, "<leader>gb", "<Cmd>Git blame<CR>")
         vim.keymap.set({'n', 'v'}, "<leader>gd", "<Cmd>Gvdiffsplit<CR>") -- Opens vimdiff. dp (diffput) do (diffobtain)
         vim.keymap.set({'n', 'v'}, "<leader>go", "<Cmd>GBrowse<cr>") -- g(it) o(open) - Open file in the browser 
-        vim.keymap.set({'n', 'v'}, "<leader>gc", "<Cmd>GCommit<cr>") -- g(it) o(open) - Open file in the browser 
         vim.keymap.set({'n', 'v'}, "<leader>gs", "<Cmd>Git<cr>") -- g(it) s(status) - Open fugitive interactive prompt
     end
 }
