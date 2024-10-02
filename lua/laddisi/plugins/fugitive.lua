b74fa4162c228df18c90779ffadb7f11efd1a699
Lucas Addisi <lucas.addisi@gmail.com>
2024-10-02 19:09:25 -0300

    Add git fugitive key bingings

--- lua/laddisi/plugins/fugitive.lua
+++ lua/laddisi/plugins/fugitive.lua
@@ -6,8 +6,9 @@ return {
     config = function()
         vim.keymap.set({'n', 'v'}, "<leader>gb", "<Cmd>Git blame<CR>")
         vim.keymap.set({'n', 'v'}, "<leader>gd", "<Cmd>Gvdiffsplit<CR>") -- Opens vimdiff. dp (diffput) do (diffobtain)
-        vim.keymap.set({'n', 'v'}, "<leader>gad", "<Cmd>git! difftool<cr>") -- g(it) a(ll) d(differences)
         vim.keymap.set({'n', 'v'}, "<leader>go", "<Cmd>GBrowse<cr>") -- g(it) o(open) - Open file in the browser 
+        vim.keymap.set({'n', 'v'}, "<leader>gc", "<Cmd>GCommit<cr>") -- g(it) o(open) - Open file in the browser 
+        vim.keymap.set({'n', 'v'}, "<leader>gs", "<Cmd>Git<cr>") -- g(it) s(status) - Open fugitive interactive prompt
     end
 }
 
