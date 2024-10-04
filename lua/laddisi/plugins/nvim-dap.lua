d4b9065517133dd79a09f592b47f86f77b296e43
Lucas Addisi <lucas.addisi@gmail.com>
2024-10-04 17:34:17 -0300

    Include 'Attach Debugger' dap configuration

--- lua/laddisi/plugins/nvim-dap.lua
+++ lua/laddisi/plugins/nvim-dap.lua
@@ -3,9 +3,19 @@ return {
     config = function ()
         local dap = require('dap')
         vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, {})
-        vim.keymap.set('n', '<F5>', dap.continue)
-        vim.keymap.set('n', '<F10>', dap.step_over)
-        vim.keymap.set('n', '<F11>', dap.step_into)
-        vim.keymap.set('n', '<F12>', dap.step_out)
+        vim.keymap.set('n', '<F7>', dap.step_into)
+        vim.keymap.set('n', '<F8>', dap.step_over)
+        vim.keymap.set('n', '<F9>', dap.continue)
+
+        dap.configurations.java = {
+            {
+                name = "Debug (Attach) - Remote";
+                request = "attach";
+                type = 'java';
+                hostname = "127.0.0.1";
+                port = 5005;
+            },
+        }
+
     end
 }
