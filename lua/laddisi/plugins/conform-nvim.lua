206f202675ccad33dd420d21545930e52c695072
Lucas <laddisi@salesforce.com>
2025-11-04 11:26:45 -0300

    Introduce FormatEnable and FormatDisable commands

--- lua/laddisi/plugins/conform-nvim.lua
+++ lua/laddisi/plugins/conform-nvim.lua
@@ -17,13 +17,34 @@ return {
             formatters = {
                 xmlformatter = { args = { "--indent", "4", "-" } },
             },
-            format_on_save = {
-                lsp_format = "fallback"
-            },
-            vim.keymap.set('n', '<leader>=', function()
-                require('conform').format()
-            end),
+            format_on_save = function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { lsp_format = "fallback", timeout_ms = 500 }
+            end,
             --log_level = vim.log.levels.DEBUG
+        })
+
+        -- Custom commands for enabling and disabling autoformat
+        -- See https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+        vim.api.nvim_create_user_command("FormatDisable", function(args)
+            if args.bang then
+                -- FormatDisable! will disable formatting just for this buffer
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, {
+            desc = "Disable autoformat-on-save",
+            bang = true,
+        })
+
+        vim.api.nvim_create_user_command("FormatEnable", function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end, {
+            desc = "Re-enable autoformat-on-save",
         })
     end
 }
